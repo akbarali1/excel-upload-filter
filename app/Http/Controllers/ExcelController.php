d2c79c3863cd3f1ea6e7aab6214fd610ff9e6a31
Akbarali <Akbarali9705@mail.ru>
2022-07-15 20:48:56 +0500

    first commit

---
+++ app/Http/Controllers/ExcelController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * Created by PhpStorm.
+ * Filename: ExcelController.php
+ * Project Name: excelfiler.loc
+ * Author: Акбарали
+ * Date: 15/07/2022
+ * Time: 20:40
+ * Github: https://github.com/akbarali1
+ * Telegram: @akbar_aka
+ * E-mail: me@akbarali.uz
+ */
+class ExcelController extends Controller
+{
+
+    public function index()
+    {
+        return view('excel.index');
+    }
+
+
+}
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post("/excel", "loginForm")->name("login");
+    Route::get("/login", "login")->name("login");
+    Route::get("/logout", "logout")->name("logout");
+});
